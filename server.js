df72be299fc2c6eb8311ed4336a3d333b17f19b1
ripmymartin <martinsild.mr@gmail.com>
2025-10-10 10:29:48 +0300

    add swagger

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 5001;
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
